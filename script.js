2638660b5900f90f97c34079dac9071455b9521d
Chinni <73129546+srikotturu@users.noreply.github.com>
2023-09-28 16:36:01 -0400

    Landing and search

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-function findStore() {
-    const zipcode = document.getElementById('zipcode').value;
-    if(!zipcode) {
-        alert('Please enter a zip code.');
-        return;
-    }
+function performSearch() {
+  const searchZip = document.getElementById('searchInput').value;
+  if (!searchZip) {
+    alert('Please enter a zip code.');
+    return;
+  }
+  if (searchZip.length !== 5 || isNaN(searchZip)) {
+    alert('Please enter a valid 5-digit zip code.');
+    return;
+  }
 
-    // Fetch store data. For simplicity, we're using dummy data
-    const stores = [{
-        name: "Daycare A",
-        zip: "12345",
-        address: "123 A Street"
-    }, {
-        name: "Daycare B",
-        zip: "67890",
-        address: "678 B Street"
-    }];
-
-    const results = stores.filter(store => store.zip === zipcode);
-    
-    const resultsDiv = document.getElementById('storeResults');
-    resultsDiv.innerHTML = '';
-
-    if(results.length === 0) {
-        resultsDiv.innerHTML = 'No stores found for the provided zip code.';
-    } else {
-        results.forEach(result => {
-            resultsDiv.innerHTML += `<div>${result.name} - ${result.address}</div>`;
-        });
-    }
+  // Redirect to results.html with search query as URL parameter
+  window.location.href = `results.html?search=${searchZip}`;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const searchParams = new URLSearchParams(window.location.search);
+  const searchZip = searchParams.get('search');
+
+  if (!searchZip) {
+    console.error('Error: search query not found in URL parameter.');
+    return;
+  }
+
+  fetch('daycares.json')
+    .then(response => response.json())
+    .then(daycares => {
+      const results = daycares.filter(daycare => daycare.zip === searchZip);
+
+      const searchResultsDiv = document.getElementById('searchResults');
+      if (!searchResultsDiv) {
+        console.error('Error: searchResults div not found.');
+        return;
+      }
+
+      searchResultsDiv.innerHTML = '';
+
+      if (results.length === 0) {
+        searchResultsDiv.innerHTML = 'No daycares found for the provided zip code.';
+      } else {
+        // Show search results with zip code in heading
+      const heading = document.createElement('h1');
+      heading.textContent = `Search Results for ${searchZip}`;
+      searchResultsDiv.appendChild(heading);
+
+        results.forEach(result => {
+          const resultDiv = document.createElement('div');
+          resultDiv.classList.add('result-container');
+          resultDiv.innerHTML = `<strong>${result.name}</strong> (${result.rating}) <br> ${result.description} <br> ${result.contact_number}`;
+          searchResultsDiv.appendChild(resultDiv);
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching daycares:', error);
+    });
+});
